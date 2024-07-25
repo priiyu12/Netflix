875058156a66d0f257e831fffa77771243aea0f1
priiyu12 <136467028+priiyu12@users.noreply.github.com>
2024-07-25 15:26:13 +0530

    Create splash.dart

---
+++ codes/splash.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Player Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: VideoPlayerScreen(),
+    );
+  }
+}
+
+class VideoPlayerScreen extends StatefulWidget {
+  @override
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController _controller;
+  late ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/showbox.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+      });
+    _chewieController = ChewieController(
+      videoPlayerController: _controller,
+      autoPlay: true,
+      looping: true,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video Player'),
+      ),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? Chewie(controller: _chewieController)
+            : CircularProgressIndicator(),
+      ),
+    );
+  }
+}
